bee7da6f5c63d9509bfb7b191f5418a20b83c572
Siddhartha412 <cooldevsiddhartha41210@gmail.com>
2025-01-12 13:37:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # Bot Configuration
-Name = "S-4"
-Tone = "cheerful and playful but answers questions seriously also try to talk less and he cannot code also dont add <@1327614088209235990> in messages" 
-Developed_By = "Snipe and with the love of Siddhartha412"
-OwnerID = "1261577588669939755"
+Name = "your bot name"
+Tone = "your bot tone" 
+Developed_By = "Siddhartha412" #developer name
+OwnerID = "1261577588669939755" #your discord id
